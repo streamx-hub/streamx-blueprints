360413cad409318c441a6cdc1fb3c12c086ab8b8
Stanisław Ruchlicki <stanislaw.ruchlicki@ds.pl>
2025-10-27 13:54:04 +0100

    Migrate xpath processing code to use quarkus-native-friendly libraries

--- services/processing/external-references-rewriter/src/main/java/com/streamx/blueprints/rewriter/finders/XmlValuesFinder.java
+++ services/processing/external-references-rewriter/src/main/java/com/streamx/blueprints/rewriter/finders/XmlValuesFinder.java
@@ -3,58 +3,65 @@ package com.streamx.blueprints.rewriter.finders;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.regex.Pattern;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathEvaluationResult;
-import javax.xml.xpath.XPathEvaluationResult.XPathResultType;
-import javax.xml.xpath.XPathExpression;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
-import javax.xml.xpath.XPathNodes;
+import org.apache.camel.CamelContext;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.language.xpath.XPathBuilder;
 import org.w3c.dom.Document;
-import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 public class XmlValuesFinder extends BaseValuesFinder {
 
-  private static final XPathFactory xpathFactory = XPathFactory.newInstance();
+  private static final CamelContext camelContext = new DefaultCamelContext();
+  private static final Pattern XPATH_FUNCTION_CALL_REGEX = Pattern
+      .compile("^([a-zA-Z_][a-zA-Z0-9_-]*)\\(.*");
 
   @Override
   public Set<String> doFindMatchingValues(String inputContent, List<String> xpathExpressions)
       throws Exception {
     String fixedInputContent = fixInputContent(inputContent);
     Document document = parseToDocument(fixedInputContent);
-    XPath xpath = xpathFactory.newXPath();
 
     Set<String> foundValues = new LinkedHashSet<>();
     for (String xpathExpression : xpathExpressions) {
-      findMatchingValues(document, xpathExpression, xpath, foundValues);
+      try (XPathBuilder xpath = new XPathBuilder(xpathExpression)) {
+        List<String> matchingValues = findMatchingValues(xpath, document);
+        matchingValues.forEach(value -> addValue(value, foundValues, xpathExpression));
+      }
     }
     return foundValues;
   }
 
-  private void findMatchingValues(Document document, String xpathExpression,
-      XPath xpath, Set<String> foundValues) throws XPathExpressionException {
-    XPathExpression compiledExpression = xpath.compile(xpathExpression);
-
-    XPathEvaluationResult<?> evaluationResult = compiledExpression.evaluateExpression(document);
-    XPathResultType type = evaluationResult.type();
-
-    if (type == XPathResultType.STRING) {
-      String value = (String) evaluationResult.value();
-      addValue(value, foundValues, xpathExpression);
-    } else if (type == XPathResultType.NODESET) {
-      XPathNodes nodes = (XPathNodes) evaluationResult.value();
-      for (Node node : nodes) {
-        String value = node.getTextContent().trim();
-        addValue(value, foundValues, xpathExpression);
-      }
-    } else {
-      throw new UnsupportedOperationException(type + " is not handled yet");
+  private List<String> findMatchingValues(XPathBuilder xpath, Document document) {
+    if (isFunctionCall(xpath.getExpressionText())) {
+      return List.of(xpath.evaluate(camelContext, document));
     }
+
+    Object result = xpath.evaluate(camelContext, document, Object.class);
+
+    if (result instanceof String value) {
+      return List.of(value);
+    }
+
+    if (result instanceof NodeList nodes) {
+      List<String> values = new ArrayList<>();
+      for (int i = 0; i < nodes.getLength(); i++) {
+        values.add(nodes.item(i).getTextContent().trim());
+      }
+      return values;
+    }
+
+    throw new UnsupportedOperationException(result.getClass() + " is not handled yet");
+  }
+
+  private boolean isFunctionCall(String xpathExpression) {
+    return XPATH_FUNCTION_CALL_REGEX.matcher(xpathExpression).matches();
   }
 
   private Document parseToDocument(String input) throws Exception {
