fbaf108ab96fc32a3a5b086e0432ad33678ad67a
Stanisław Ruchlicki <165791712+stan-r-ds@users.noreply.github.com>
2025-11-17 16:30:12 +0100

    DXP-2425 Add QuarkusIntegrationTest for optimized-image-generator service (#63)

--- services/processing/optimized-images-generator/src/test/java/com/streamx/blueprints/image/generator/OptimizedImagesGeneratorIT.java
+++ services/processing/optimized-images-generator/src/test/java/com/streamx/blueprints/image/generator/OptimizedImagesGeneratorIT.java
@@ -1,4 +1,4 @@
-package com.streamx.blueprints.event.converter;
+package com.streamx.blueprints.image.generator;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.post;
@@ -10,9 +10,9 @@ import static org.awaitility.Awaitility.await;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.verification.LoggedRequest;
 import com.streamx.blueprints.cloudevents.utils.CloudEventUtils;
-import com.streamx.blueprints.data.Data;
-import com.streamx.blueprints.data.IndexableResource;
-import com.streamx.blueprints.event.converter.EventConverterIT.WireMockProfile;
+import com.streamx.blueprints.data.Asset;
+import com.streamx.blueprints.data.OptimizedAsset;
+import com.streamx.blueprints.image.generator.OptimizedImagesGeneratorIT.WireMockProfile;
 import com.streamx.reactive.messaging.http.CloudEventJsonDeserializer;
 import com.streamx.reactive.messaging.http.CloudEventJsonSerializer;
 import io.cloudevents.CloudEvent;
@@ -21,10 +21,12 @@ import io.quarkus.test.junit.QuarkusIntegrationTest;
 import io.quarkus.test.junit.QuarkusTestProfile;
 import io.quarkus.test.junit.TestProfile;
 import io.vertx.core.buffer.Buffer;
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicReference;
+import org.apache.commons.io.FileUtils;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
@@ -38,33 +40,37 @@ import org.junit.jupiter.api.Test;
 @QuarkusIntegrationTest
 @ConnectWireMock
 @TestProfile(WireMockProfile.class)
-public class EventConverterIT {
+public class OptimizedImagesGeneratorIT {
 
-  private static final String RESOURCES_URL = "http://localhost:8081/" + Channels.RESOURCES;
-  private static final String INDEXABLE_RESOURCES_ENDPOINT = "/" + Channels.INDEXABLE_RESOURCES;
+  private static final String INCOMING_ASSETS_URL = "http://localhost:8081/"
+                                                    + Channels.INCOMING_ASSETS;
+  private static final String OPTIMIZED_ASSETS_ENDPOINT = "/" + Channels.OPTIMIZED_ASSETS;
 
   // will be injected automatically when the test class is annotated with @ConnectWireMock
   WireMock wiremock;
 
   @BeforeAll
   static void setEventSource() {
-    System.setProperty("quarkus.application.name", EventConverterIT.class.getSimpleName());
+    System.setProperty("quarkus.application.name",
+        OptimizedImagesGeneratorIT.class.getSimpleName());
   }
 
   @BeforeEach
   void setupEndpointForReceivingOutgoingEvents() {
-    wiremock.register(post(urlEqualTo(INDEXABLE_RESOURCES_ENDPOINT))
+    wiremock.register(post(urlEqualTo(OPTIMIZED_ASSETS_ENDPOINT))
         .willReturn(aResponse().withStatus(202)));
   }
 
   @Test
-  void shouldConvertDataToIndexableResource() throws IOException {
-    Data data = new Data("{\"key\": \"value\"}", "type");
-    CloudEvent sourceEvent = createPublishDataEvent(data);
+  void shouldGenerateOptimizedImage() throws IOException {
+    File testImageFile = new File("src/test/resources/ds.png");
+    String key = testImageFile.getName();
+    Asset asset = new Asset(FileUtils.readFileToByteArray(testImageFile), "test-image");
+    CloudEvent sourceEvent = createPublishAssetEvent(key, asset);
     String serializedEvent = new CloudEventJsonSerializer().serialize(sourceEvent).toString();
 
     try (CloseableHttpClient http = HttpClients.createDefault()) {
-      HttpPost post = new HttpPost(RESOURCES_URL);
+      HttpPost post = new HttpPost(INCOMING_ASSETS_URL);
       post.setEntity(new StringEntity(serializedEvent));
       CloseableHttpResponse response = http.execute(post);
       assertThat(response.getStatusLine().getStatusCode()).isEqualTo(HttpStatus.SC_ACCEPTED);
@@ -73,18 +79,18 @@ public class EventConverterIT {
     LoggedRequest response = waitForResponseRequest();
     byte[] body = response.getBody();
     CloudEvent outgoingEvent = new CloudEventJsonDeserializer().deserialize(Buffer.buffer(body));
-    assertOutgoingEvent(outgoingEvent, sourceEvent, data);
+    assertOutgoingEvent(outgoingEvent, sourceEvent, asset);
   }
 
-  private static CloudEvent createPublishDataEvent(Data data) {
-    return CloudEventUtils.eventWithData("key", Data.TYPE_PUBLISHED, data);
+  private static CloudEvent createPublishAssetEvent(String key, Asset asset) {
+    return CloudEventUtils.eventWithData(key, Asset.TYPE_PUBLISHED, asset);
   }
 
   private static LoggedRequest waitForResponseRequest() {
     AtomicReference<LoggedRequest> result = new AtomicReference<>();
     await().untilAsserted(() -> {
       List<LoggedRequest> requests = WireMock.findAll(
-          postRequestedFor(urlEqualTo(INDEXABLE_RESOURCES_ENDPOINT)));
+          postRequestedFor(urlEqualTo(OPTIMIZED_ASSETS_ENDPOINT)));
       assertThat(requests).hasSize(1);
       result.set(requests.getFirst());
     });
@@ -92,18 +98,21 @@ public class EventConverterIT {
   }
 
   private static void assertOutgoingEvent(CloudEvent outgoingEvent, CloudEvent sourceEvent,
-      Data sourceData) {
+      Asset sourceAsset) {
     assertThat(outgoingEvent.getId()).isNotEqualTo(sourceEvent.getId());
-    assertThat(outgoingEvent.getSource()).asString().isEqualTo("event-converter");
-    assertThat(outgoingEvent.getSubject()).isEqualTo(sourceEvent.getSubject());
-    assertThat(outgoingEvent.getType()).isEqualTo(IndexableResource.TYPE_PUBLISHED);
+    assertThat(outgoingEvent.getSource()).asString().isEqualTo("optimized-images-generator");
+    assertThat(outgoingEvent.getSubject())
+        .isNotEqualTo(sourceEvent.getSubject())
+        .endsWith("-optimized.webp");
+    assertThat(outgoingEvent.getType()).isEqualTo(OptimizedAsset.TYPE_PUBLISHED);
     assertThat(outgoingEvent.getTime()).isEqualTo(sourceEvent.getTime());
     assertThat(outgoingEvent.getDataContentType()).isEqualTo(sourceEvent.getDataContentType());
 
-    var outgoingResource = CloudEventUtils.getData(outgoingEvent, IndexableResource.class);
+    var outgoingResource = CloudEventUtils.getData(outgoingEvent, OptimizedAsset.class);
     assertThat(outgoingResource).isNotNull();
-    assertThat(outgoingResource.getType()).isEqualTo(sourceData.getType());
-    assertThat(outgoingResource.getContent()).isEqualTo(sourceData.getContent());
+    assertThat(outgoingResource.getType()).isEqualTo(sourceAsset.getType());
+    assertThat(outgoingResource.getContent()).isNotEqualTo(sourceAsset.getContent());
+    assertThat(outgoingResource.getOriginalPath()).isEqualTo(sourceEvent.getSubject());
   }
 
   public static class WireMockProfile implements QuarkusTestProfile {
@@ -111,9 +120,11 @@ public class EventConverterIT {
     @Override
     public Map<String, String> getConfigOverrides() {
       return Map.of(
-          "mp.messaging.outgoing." + Channels.INDEXABLE_RESOURCES + ".url",
+          "mp.messaging.outgoing." + Channels.OPTIMIZED_ASSETS + ".url",
           "http://" + getContainerLocalhost() + ":${quarkus.wiremock.devservices.port}"
-          + INDEXABLE_RESOURCES_ENDPOINT
+          + OPTIMIZED_ASSETS_ENDPOINT,
+          "streamx.blueprints.optimized-images-generator.processed-image-path-pattern",
+          ".*"
       );
     }
   }
