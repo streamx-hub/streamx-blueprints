87b950e88b8a89ea00d2ad434b1c1796abe17abe
Stanisław Ruchlicki <165791712+stan-r-ds@users.noreply.github.com>
2025-11-17 16:48:45 +0100

    DXP-2425 Add QuarkusIntegrationTest for external-references-rewriter service (#67)

---
+++ services/processing/external-references-rewriter/src/test/java/com/streamx/blueprints/rewriter/ExternalReferencesRewriterIT.java
@@ -0,0 +1,104 @@
+package com.streamx.blueprints.rewriter;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.streamx.blueprints.cloudevents.utils.CloudEventUtils;
+import com.streamx.blueprints.data.DownloadRequest;
+import com.streamx.blueprints.data.Page;
+import com.streamx.blueprints.rewriter.ExternalReferencesRewriterIT.IntegrationTestProfile;
+import com.streamx.blueprints.test.integration.BaseQuarkusIntegrationTest;
+import io.cloudevents.CloudEvent;
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+import io.quarkus.test.junit.QuarkusTestProfile;
+import io.quarkus.test.junit.TestProfile;
+import java.io.IOException;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+@QuarkusIntegrationTest
+@TestProfile(IntegrationTestProfile.class)
+public class ExternalReferencesRewriterIT extends BaseQuarkusIntegrationTest {
+
+  private static final String SOURCE_PAGE_TYPE = "pages/sample-page";
+  private static final String EXTERNAL_PAGE_TYPE = "pages/external";
+  private static final String EXTERNAL_WEB_RESOURCE_TYPE = "web-resources/external";
+  private static final String EXTERNAL_ASSET_TYPE = "assets/external";
+
+  @Test
+  void shouldRewriteExternalReferences() throws IOException {
+    // given
+    String pageContent = """
+        <html>
+          <body>
+            <img src='https://www.google.com/logo.jpg' />
+          </body>
+        </html>""";
+    String key = "/pages/contact.html";
+
+    Page page = new Page(pageContent, SOURCE_PAGE_TYPE);
+    CloudEvent sourceEvent = CloudEventUtils.eventWithData(key, Page.TYPE_PUBLISHED, page);
+
+    // when
+    sendEvent(sourceEvent, Channels.INCOMING_RESOURCES);
+
+    // then: assert page has a rewritten link
+    CloudEvent outgoingPageEvent = waitForResponseEvent(Channels.OUTGOING_RESOURCES);
+    assertOutgoingPageEvent(outgoingPageEvent, sourceEvent, page);
+
+    // and: download request should be sent
+    CloudEvent outgoingDownloadRequestEvent = waitForResponseEvent(Channels.DOWNLOAD_REQUESTS);
+    assertOutgoingDownloadRequestEvent(outgoingDownloadRequestEvent, sourceEvent);
+  }
+
+  private static void assertOutgoingPageEvent(CloudEvent outgoingEvent, CloudEvent sourceEvent,
+      Page sourceData) {
+    assertThat(outgoingEvent.getId()).isNotEqualTo(sourceEvent.getId());
+    assertThat(outgoingEvent.getSource()).hasPath("external-references-rewriter");
+    assertThat(outgoingEvent.getSubject()).isEqualTo(sourceEvent.getSubject());
+    assertThat(outgoingEvent.getType()).isEqualTo(Page.TYPE_PUBLISHED);
+    assertThat(outgoingEvent.getTime()).isEqualTo(sourceEvent.getTime());
+
+    var outgoingResource = CloudEventUtils.getData(outgoingEvent, Page.class);
+    assertThat(outgoingResource).isNotNull();
+    assertThat(outgoingResource.getType()).isEqualTo(sourceData.getType());
+    assertThat(outgoingResource.getContentAsString()).isEqualTo("""
+        <html>
+          <body>
+            <img src='/https_www.google.com/logo.jpg' />
+          </body>
+        </html>""");
+  }
+
+  private static void assertOutgoingDownloadRequestEvent(CloudEvent outgoingEvent,
+      CloudEvent sourceEvent) {
+    assertThat(outgoingEvent.getId()).isNotEqualTo(sourceEvent.getId());
+    assertThat(outgoingEvent.getSource()).hasPath("external-references-rewriter");
+    assertThat(outgoingEvent.getSubject()).isEqualTo("/https_www.google.com/logo.jpg");
+    assertThat(outgoingEvent.getType()).isEqualTo(DownloadRequest.EVENT_TYPE);
+    assertThat(outgoingEvent.getTime()).isNotEqualTo(sourceEvent.getTime());
+
+    var outgoingResource = CloudEventUtils.getData(outgoingEvent, DownloadRequest.class);
+    assertThat(outgoingResource).isNotNull();
+    assertThat(outgoingResource.url()).isEqualTo("https://www.google.com/logo.jpg");
+    assertThat(outgoingResource.emitKey()).isEqualTo("/https_www.google.com/logo.jpg");
+    assertThat(outgoingResource.emittedPageType()).isEqualTo(EXTERNAL_PAGE_TYPE);
+    assertThat(outgoingResource.emittedWebResourceType()).isEqualTo(EXTERNAL_WEB_RESOURCE_TYPE);
+    assertThat(outgoingResource.emittedAssetType()).isEqualTo(EXTERNAL_ASSET_TYPE);
+  }
+
+  public static class IntegrationTestProfile implements QuarkusTestProfile {
+
+    @Override
+    public Map<String, String> getConfigOverrides() {
+      String basePropertyPath = "streamx.blueprints.external-references-rewriter.";
+      return propertiesForOutgoingChannels()
+          .put(basePropertyPath + "html-external-resource-xpath-selectors", "//img/@src")
+          .put(basePropertyPath + "base-url-for-relative-paths", "https://www.streamx.dev")
+          .put(basePropertyPath + "processable-payload-types", SOURCE_PAGE_TYPE)
+          .put(basePropertyPath + "emitted-page-type", EXTERNAL_PAGE_TYPE)
+          .put(basePropertyPath + "emitted-web-resource-type", EXTERNAL_WEB_RESOURCE_TYPE)
+          .put(basePropertyPath + "emitted-asset-type", EXTERNAL_ASSET_TYPE)
+          .build();
+    }
+  }
+}
