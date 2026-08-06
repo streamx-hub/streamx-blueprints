c2b2b08da3cfeba83fc342b982f4732cf717569b
Kamil Chojak <kamil.chojak@ds.pl>
2026-08-06 13:11:59 +0200

    SXPR-315 Remove not used code

--- services/processing/indexable-resources-producer/src/main/java/com/streamx/blueprints/index/collectors/html/impl/AbstractHtmlCollector.java
+++ services/processing/indexable-resources-producer/src/main/java/com/streamx/blueprints/index/collectors/html/impl/AbstractHtmlCollector.java
@@ -1,13 +1,11 @@
 package com.streamx.blueprints.index.collectors.html.impl;
 
 import java.util.Collection;
-import java.util.List;
 import java.util.Map;
 import java.util.function.BiFunction;
 import java.util.stream.Collectors;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 
 public abstract class AbstractHtmlCollector {
 
@@ -22,19 +20,5 @@ public abstract class AbstractHtmlCollector {
             Map.Entry::getKey,
             Map.Entry::getValue
         ));
-  }
-
-  protected static String normalizeKey(String key) {
-    return key.trim()
-        .toLowerCase()
-        .replace("-", "_");
-  }
-
-  protected static String findFirst(Element element, List<String> attributesNames) {
-    return attributesNames.stream()
-        .filter(element::hasAttr)
-        .map(element::attr)
-        .findFirst()
-        .orElse(null);
   }
 }
